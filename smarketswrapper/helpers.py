708a5c7fd03f29ef51ff720a1026dab2ac8265ab
BrianRyan94 <brian.ryan@ucdconnect.ie>
2021-01-22 10:42:38 +0000

    Added functions for returning dataframe of odds/dataframe of last traded prices

--- smarketswrapper/helpers.py
+++ smarketswrapper/helpers.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+import pandas as pd
 
 baseurl = "https://api.smarkets.com/v3/"
 
@@ -32,3 +34,65 @@ def timestamp_todatetime(x):
 def datetime_totimestamp(x):
     x = datetime.datetime.strftime(x, "%Y-%m-%dT%H:%M:%SZ")
     return x
+
+def odds_transformer(x):
+    return float(x)/10000
+
+def parse_odds_to_df(data):
+    contract_ids = list(data.keys())
+    df_dict = {"ID": []}
+    for i in range(3):
+        df_dict['BACK' + str(i)] = []
+        df_dict['LAY' + str(i)] = []
+        df_dict['BACKSIZE' + str(i)] = []
+        df_dict['LAYSIZE' + str(i)] = []
+
+    for competitor in contract_ids:
+
+        lays = data[competitor]['offers']
+        backs = data[competitor]['bids']
+
+        df_dict['ID'].append(competitor)
+
+        for i in range(3):
+            try:
+                back_packet = backs[i]
+                px = back_packet['price']
+                sz = back_packet['quantity']
+            except:
+                px = 0
+                sz = 0
+
+            df_dict['BACK' + str(i)].append(odds_transformer(px))
+            df_dict['BACKSIZE' + str(i)].append(size_transformer(sz, px))
+
+            try:
+                lay_packet = lays[i]
+                px = lay_packet['price']
+                sz = lay_packet['quantity']
+            except:
+                px = 0
+                sz = 0
+
+            df_dict['LAY' + str(i)].append(odds_transformer(px))
+            df_dict['LAYSIZE' + str(i)].append(size_transformer(sz, px))
+
+    return pd.DataFrame(df_dict).sort_values(by='BACK1', ascending=False)
+
+def parse_trades_to_df(data):
+    df_dict = {}
+    contractids = [x['contract_id'] for x in data]
+    prices = [float(x['last_executed_price']) / 100 for x in data]
+    times = [timestamp_todatetime(x['timestamp']) for x in data]
+
+    df_dict['ID'] = contractids
+    df_dict['price'] = prices
+    df_dict['timestamp'] = times
+
+    return pd.DataFrame(df_dict).sort_values(by='timestamp', ascending=False)
+
+def size_transformer(sz, px):
+    try:
+        return (float(sz)*float(px))/100000000
+    except:
+        return 0
--- smarketswrapper/marketdata.py
+++ smarketswrapper/marketdata.py
@@ -3,3 +3,56 @@ import pandas as pd
 
 
 
+def get_live_odds(sesstoken, marketid, type):
+
+    if type=="quote":
+         endpoint = "markets/{0}/quotes/".format(str(marketid))
+    elif type=="trade":
+         endpoint = "markets/{0}/last_executed_prices/".format(str(marketid))
+
+    headers = {"Authorization": "Session-Token {0}".format(sesstoken)}
+
+    result = helpers.data_req(endpoint, headers)
+    success = False
+    if result.status_code==200:
+        data = result.json()
+        if type=="trade":
+            if 'last_executed_prices' in data:
+                data = data['last_executed_prices']
+                if str(marketid) in data and len(data[str(marketid)])>0:
+                    success = True
+                    data = data[str(marketid)]
+            if success==False:
+                details = "Response received but no trades for market id {0} found".apply(marketid)
+        elif type=="quote":
+            if len(data)>0:
+                success = True
+            else:
+                success = False
+                details = "Response received but no quotes for market id {0} found".apply(marketid)
+
+        if success:
+
+            if type=="quote":
+                details = helpers.parse_odds_to_df(data)
+
+            elif type=="trade":
+                details = helpers.parse_trades_to_df(data)
+
+    return success, details
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
