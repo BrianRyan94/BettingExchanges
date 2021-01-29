d5c4c8b97ceb99d2608a4e6168ddd761a043f7b3
BrianRyan94 <brian.ryan@ucdconnect.ie>
2021-01-29 14:00:23 +0000

    Removed .idea

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="betfair_smarkets_simple",
+    version="0.0.1",
+    author="Brian Ryan",
+    author_email = "brian.ryan@ucdconnect.ie",
+    description="Wrappers for smarkets/betfair APIs",
+    packages=['betfairwrapper','smarketswrapper'],
+    url="https://github.com/BrianRyan94/BettingExchanges",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
