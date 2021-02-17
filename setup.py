2fd181b9262fb2ab8223b692eb69b515c14d6d51
BrianRyan94 <brian.ryan@ucdconnect.ie>
2021-02-17 23:12:55 +0000

    Versioned up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="betfair_smarkets_simple",
-    version="0.0.4",
+    version="0.0.5",
     author="Brian Ryan",
     author_email = "brian.ryan@ucdconnect.ie",
     description="Wrappers for smarkets/betfair APIs",
