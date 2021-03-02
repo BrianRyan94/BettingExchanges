65f45d7346786fa7c49678155ba8cd76d8b62138
BrianRyan94 <brian.ryan@ucdconnect.ie>
2021-03-02 07:50:09 +0000

    Change start datetime to timestamp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="betfair_smarkets_simple",
-    version="0.1.9",
+    version="0.2.0",
     author="Brian Ryan",
     author_email = "brian.ryan@ucdconnect.ie",
     description="Wrappers for smarkets/betfair APIs",
